425eb79b02659b13697b4aefe5aa426b31dad86d
marek15snk <48221433+marek15snk@users.noreply.github.com>
2019-03-04 21:34:10 +0100

    Pierwszy program Kurs1. Tablica Args

--- Kurs1/Program.cs
+++ Kurs1/Program.cs
@@ -10,6 +10,16 @@ namespace Kurs1
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            Console.WriteLine("Witaj, " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj,  " + item);
+            //}
+           
+          
+           
         }
     }
 }
